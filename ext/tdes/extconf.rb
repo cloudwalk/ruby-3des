6a6a84c81a96ff1ded481ea8d0244b5ec9a0d7f0
Lucas D. Lima <lucas.lima@cloudwalk.io>
2024-09-03 09:55:07 -0300

    fix: remove return type from have_func

--- ext/tdes/extconf.rb
+++ ext/tdes/extconf.rb
@@ -13,7 +13,7 @@ with_cflags(cflags) do
 
     # On MacOS, have_func does not work well because the default compiler is clang instead of gcc
     unless RUBY_PLATFORM.downcase.include? 'darwin'
-      abort 'missing DES_set_key_checked' unless have_func('int DES_set_key_checked', 'openssl/des.h')
+      abort 'missing DES_set_key_checked' unless have_func('DES_set_key_checked', 'openssl/des.h')
       abort 'missing DES_ecb3_encrypt' unless have_func('DES_ecb3_encrypt', 'openssl/des.h')
       # Check for a symbol/function specific to OpenSSL 3.x
       abort 'OpenSSL 3.x required (missing EVP_Cipher function)' unless have_func('EVP_Cipher')
